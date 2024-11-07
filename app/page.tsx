e4fef56e69736fed830127e022a8add8e6cd4aed
Fábio Rassele <fabiiohenrique.or@gmail.com>
2024-11-07 00:13:44 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="w-screen items-center bg-cyan-50 p-5 text-red-500">
+      Hello World
+    </h1>
+  );
 }
