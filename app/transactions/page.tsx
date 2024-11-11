8fcb140a022dd8d6fa63aa01bd69c175aabb511c
Fábio Rassele <fabiiohenrique.or@gmail.com>
2024-11-10 22:46:29 -0300

    feat: add user authentication to dashboard data fetching and simplify transaction data handling

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -21,15 +21,13 @@ const TransactionsPage = async () => {
     <>
       <Navbar />
       <div className="space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
         <ScrollArea>
-          <DataTable
-            columns={transactionColumns}
-            data={JSON.parse(JSON.stringify(transactions))}
-          />
+          <DataTable columns={transactionColumns} data={transactions} />
         </ScrollArea>
       </div>
     </>
